67896752510d584a1411472bb773742dd8fff106
Beedy1122 <sashabeedy@gmail.com>
2025-05-01 23:46:43 +0300

    Rus comments new

--- examples/Hello_world.cpp
+++ examples/Hello_world.cpp
@@ -6,9 +6,6 @@ main(int argc, char** argv)
 std::string name;//Имя
 std::cout<<"Enter your name \n";
 std::cin>>name;
-std::cout<<Hello world from" <<name<<std::endl;//Рез
+std::cout<<Hello world from" <<name<<std::endl;//Результ
   std::string name;
-  std::cout << "Enter your name \n";
-  std::cin >> name;
-  std::cout << Hello world from " <<name<<std::endl;//res
 }
