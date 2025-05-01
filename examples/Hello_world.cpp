29286d3c2d4fb7d63567f2460eea7c34ac14c1ee
Beedy1122 <sashabeedy@gmail.com>
2025-05-01 23:41:34 +0300

    Styled Hello World

--- examples/Hello_world.cpp
+++ examples/Hello_world.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <string>
-int main(int argc, char** argv)
+int
+main(int argc, char** argv)
 {
 std::string name;//Имя
 std::cout<<"Enter your name \n";
 std::cin>>name;
 std::cout<<Hello world from" <<name<<std::endl;//Рез
+  std::string name;
+  std::cout << "Enter your name \n";
+  std::cin >> name;
+  std::cout << Hello world from " <<name<<std::endl;//res
 }
