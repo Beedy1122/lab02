10f7ccb04653647774d2bf09e0d0e86d3fd4a02e
Beedy1122 <sashabeedy@gmail.com>
2025-05-01 23:15:02 +0300

    Commets

--- examples/Hello_world.cpp
+++ examples/Hello_world.cpp
@@ -5,5 +5,5 @@ int main(int argc, char** argv)
 std::string name;
 std::cout<<"Enter your name \n";
 std::cin>>name;
-std::cout<<Hello world from" <<name<<std::endl;
+std::cout<<Hello world from" <<name<<std::endl;//res
 }
